f58d03d264b0874e9666cf51a2e17848447d4160
Anthony Filgueira <anthonyfilgueira@innodite.com>
2023-06-19 22:37:41 -0500

    install laravel modules and create module User

---
+++ Modules/User/Routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\User\Http\Controllers\UserController;
+
+Route::prefix('users')->group(function() {
+    Route::get('/', [UserController::class,'index'])->name('user.index');
+    Route::post('/', [UserController::class,'store'])->name('user.store');
+});
---
+++ Modules/User/Tests/Unit/UserModuleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\User\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserModuleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_creates_a_new_user():void
+    {
+        $this->post(route('user.store'),[
+            'name'=>'',
+            'email'=>'',
+            'password'=>'',
+        ])->assertSee('Procesando informacion');
+
+    }
+}
