3c1081c4147b81a22f0c100eae4e7dac8880f427
Anthony Filgueira <anthonyfilgueira@innodite.com>
2023-07-08 21:14:55 -0500

    adjust user module test

--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\User\Http\Controllers\UserController;
 
 Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class,'index'])->name('user.index');
-    Route::post('/', [UserController::class,'store'])->name('user.store');
+    Route::get('/', [UserController::class,'index'])->name('users.index');
+    Route::post('/', [UserController::class,'store'])->name('users.store');
 });
--- Modules/User/Tests/Unit/UserModuleTest.php
+++ Modules/User/Tests/Unit/UserModuleTest.php
@@ -10,7 +10,7 @@ class UserModuleTest extends TestCase
 {
     public function test_creates_a_new_user(): void
     {
-        $this->post(route('user.store'), [
+        $this->post(route('users.store'), [
             'name' => '',
             'email' => '',
             'password' => '',
